5f7c4ae35afa7abf0c840413d17079b607a879ca
Gyel-Tshen <gyeltshen7777@gmail.com>
2021-04-04 02:08:06 +0600

    chnaged

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,28 @@
 class User < ApplicationRecord
   
-  acts_as_token_authenticatable
+  # acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   has_many :participations
   has_many :activities, :through => :participations
   has_many :assignments  
-  has_many :roles, through: :assignments
+  has_many :user_roles
+  has_many :roles, through: :user_roles
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  rolify
-  def generate_new_authentication_token
-    token = User.generate_unique_secure_token 
-    update_attribute :authentication_token, token
-  end
+  
+  # def generate_new_authentication_token
+  #   token = User.generate_unique_secure_token 
+  #   update_attribute :authentication_token, token
+  # end
 
   def role?(role)  
     roles.any? { |r| r.name.underscore.to_sym == role }  
   end 
 
-  after_create :assign_default_role
+  #after_create :assign_default_role
 
   def assign_default_role
     if User.count == 1
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_180701) do
+ActiveRecord::Schema.define(version: 2021_04_03_185243) do
 
   create_table "activities", force: :cascade do |t|
     t.string "aname"
@@ -64,6 +64,15 @@ ActiveRecord::Schema.define(version: 2021_03_18_180701) do
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
+  create_table "user_roles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "role_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["role_id"], name: "index_user_roles_on_role_id"
+    t.index ["user_id"], name: "index_user_roles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -73,6 +82,7 @@ ActiveRecord::Schema.define(version: 2021_03_18_180701) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "authentication_token", limit: 30
@@ -96,4 +106,6 @@ ActiveRecord::Schema.define(version: 2021_03_18_180701) do
   add_foreign_key "participations", "activities"
   add_foreign_key "participations", "users"
   add_foreign_key "user_profiles", "users"
+  add_foreign_key "user_roles", "roles"
+  add_foreign_key "user_roles", "users"
 end
